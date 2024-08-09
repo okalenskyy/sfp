65fbe35ed99b70cb59b1eef8fdb7d05690ea1c59
Oleg Kalenskyy <57141635+okalenskyy@users.noreply.github.com>
2024-08-09 17:28:18 +0000

    sidebar

--- main_app.py
+++ main_app.py
@@ -117,7 +117,7 @@ with st.sidebar:
     for i in range(int(st.number_input('Num:'))): foo()
     if st.sidebar.selectbox('I:',['f']) == 'f':
         my_slider_val = st.slider('Quinn Mallory', 1, 88)
-        st.write(slider_val)
+        st.write(my_slider_val)
 
 if tabs =='Dashboard':
     st.title("Navigation Bar")
