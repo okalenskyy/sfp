3f158ce19d2f532c3159a3d235d4178c7e0e96fb
Oleg Kalenskyy <57141635+okalenskyy@users.noreply.github.com>
2024-08-09 17:27:44 +0000

    sidebar

--- main_app.py
+++ main_app.py
@@ -115,9 +115,9 @@ with st.sidebar:
     
     # Use widgets' returned values in variables
     for i in range(int(st.number_input('Num:'))): foo()
-    if st.sidebar.selectbox('I:',['f']) == 'f': b()
-    my_slider_val = st.slider('Quinn Mallory', 1, 88)
-    # st.write(slider_val)
+    if st.sidebar.selectbox('I:',['f']) == 'f':
+        my_slider_val = st.slider('Quinn Mallory', 1, 88)
+        st.write(slider_val)
 
 if tabs =='Dashboard':
     st.title("Navigation Bar")
