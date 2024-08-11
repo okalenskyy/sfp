579cff8250a6d814e05e25821a0381b9f59a65a3
Oleg Kalenskyy <57141635+okalenskyy@users.noreply.github.com>
2024-08-11 12:00:58 +0000

    test

--- main_app.py
+++ main_app.py
@@ -20,11 +20,11 @@ import json
 
 # Set the title and favicon that appear in the Browser's tab bar.
 
-st.set_page_config(
-    page_title='Stocks Forecast Playground',
-    page_icon=':chart:', # This is an emoji shortcode. Could be a URL too.
-    layout='wide'
-)
+# st.set_page_config(
+#     page_title='Stocks Forecast Playground',
+#     page_icon=':chart:', # This is an emoji shortcode. Could be a URL too.
+#     layout='wide'
+# )
 
 
 # -----------------------------------------------------------------------------
