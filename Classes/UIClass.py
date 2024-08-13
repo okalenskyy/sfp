d27dad96974c31004c413590ce58988dbd96943e
Oleg Kalenskyy <57141635+okalenskyy@users.noreply.github.com>
2024-08-13 20:51:39 +0000

    w

--- Classes/UIClass.py
+++ Classes/UIClass.py
@@ -2,6 +2,9 @@ import json
 from pathlib import Path
 import pandas as pd
 
+from Classes.YahooTickerClass import YahooTicker
+from lib.models import rnn_model, lstm_horizon_model
+
 class UI():
     """
     UI handler Class
@@ -19,7 +22,7 @@ class UI():
         self.subtitle:str = self.config['subtitle']  
 
         self._selected_ticker:str = ''
-        self.selected_model:str = ''
+        self._selected_model:str = ''
         self.pred_days:int=1 
 
         self.begin_date:str = ''
@@ -33,10 +36,17 @@ class UI():
         self._selected_ticker_sector:str = ''
         self._selected_ticker_industry:str = ''
 
-        self.y_pred_df:pd.DataFrame = pd.DataFrame()
-        self.y_test_df:pd.DataFrame = pd.DataFrame()
-        self.sc  = {}
-        
+        self._y_pred_df:pd.DataFrame = pd.DataFrame()
+        self._y_test_df:pd.DataFrame = pd.DataFrame()
+        self._sc  = {}
+
+        self._X_train:pd.DataFrame = pd.DataFrame()
+        self._y_train:pd.DataFrame = pd.DataFrame() 
+        self._X_test:pd.DataFrame = pd.DataFrame()
+        self._y_test:pd.DataFrame = pd.DataFrame()
+
+        self.model:object
+                 
     def _get_ticker_property(self, ):
         return()
         #selected_ticker
@@ -56,6 +66,31 @@ class UI():
         self.selected_ticker_sector = self.tickers_df[self.tickers_df.Symbol == value].Sector.values[0]
         self.selected_ticker_industry = self.tickers_df[self.tickers_df.Symbol == value].Industry.values[0]
 
+    @property
+    def selected_model(self):
+        return self._selected_model
+
+    @selected_model.setter
+    def selected_model(self, value):
+        if not isinstance(value, str):
+            raise TypeError("selected_model must be a str")
+        self._selected_model = value
+        match value:
+            case 'LSTM':
+                self.model = lstm_horizon_model(self.X_train, selfy_train)      
+            case 'RNN'
+                self.model = rnn_model(self.X_train)      
+            # If an exact match is not confirmed, this last case will be used if provided
+            case _:
+                raise Exception(f'Sorry, but {Value} model not yet implemented.' )
+# ---------
+
+# def setup_lstm(X_train, y_train):
+#     lstm_model = lstm_horizon_model(X_train, y_train)
+#     lstm_model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.3)
+#     return lstm_model
+
+# ---------
     @property
     def selected_ticker_name(self):
         return self._selected_ticker_name
@@ -96,7 +131,62 @@ class UI():
     def selected_ticker_industry(self, value):
         self._selected_ticker_industry=value
 
+    @property
+    def y_pred_df(self):
+        return self._y_pred_df
+        
+    @y_pred_df.setter
+    def y_pred_df(self, value):
+            self._y_pred_df=value
+
+    @property
+    def y_test_df(self):
+        return self._y_test_df
+        
+    @y_test_df.setter
+    def y_test_df(self, value):
+            self._y_test_df=value
+        
+    @property
+    def sc(self):
+        return self._sc
+        
+    @sc.setter
+    def sc(self, value):
+            self._sc=value
+        
+    @property
+    def X_train(self):
+        return self._X_train
+        
+    @X_train.setter
+    def X_train(self, value):
+            self._X_train=value
       
+    @property
+    def y_train(self):
+        return self._y_train
+        
+    @y_train.setter
+    def y_train(self, value):
+            self._y_train=value
+
+    @property
+    def X_test(self):
+        return self._X_test
+        
+    @X_test.setter
+    def X_test(self, value):
+            self._X_test=value
+
+    @property
+    def y_test(self):
+        return self._y_test
+        
+    @y_test.setter
+    def y_test(self, value):
+            self._y_test=value
+
     def get_configuration(self):
         config_file_path = Path(__file__).parent.parent/'config/config.json'
         with open(config_file_path) as json_file:
@@ -109,13 +199,6 @@ class UI():
         self.tickers_df.key = 'Symbol'
         return self.tickers_df
 
-    # def get_models_list(self):
-    #     tickers_file_path = Path(__file__).parent/'data/nasdaq_tickers.csv'
-    #     # Fetch a list of stock tickers
-    #     tickers_df = pd.read_csv(tickers_file_path)
-    #     tickers_df.key = 'Symbol'
-    #     return tickers_df
-
     def construct_sidebar(self):
         # Load the tickers
         # self.tickers_df = 
@@ -125,3 +208,14 @@ class UI():
         self.models = self.config['models'].split(',')
         self.icons = self.config['model_icons'].split(',')[:len(self.models)]
         self.predict_days = self.config['predict_days']
+
+    def fetch_data(self, ticker:str, start_date:str, end_date:str):
+        # Create an instance of the class
+        self.SelectedTicker = YahooTicker(ticker, start_date, end_date)
+        # Call the fetch_data() method
+        self.SelectedTicker.fetch_data()
+    
+    def prepare_datasets(self, training_data_percent:float = 0.2, n_lags:int=60, predict_days:int=0, target_column:str=None, extra_features=[], reshape_for_lstm:bool=False):
+        self.X_train, self.y_train, self.X_test, self.y_test, self.sc = self.SelectedTicker.prepare_data_feat_step(n_lags = 1, training_data_percent = 0.3, target_column='Open',extra_features=['Close'], reshape_for_lstm=True) #.   'Close'
+        
+        
--- main_app.py
+++ main_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 from pathlib import Path
 import yfinance as yf
 
-from lib.models import rnn_model, lstm_horizon_model
-from Classes.YahooTickerClass import YahooTicker
+# from lib.models import rnn_model, lstm_horizon_model
+# from Classes.YahooTickerClass import YahooTicker
 from Classes.UIClass import UI
 
 from streamlit_pills import pills
@@ -14,51 +14,6 @@ from st_on_hover_tabs import on_hover_tabs
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
-
-# @st.cache_data
-# def load_css(file_name):
-#     with open(file_name) as f:
-#         css = f.read()
-#     return css
-
-
-def fetch_data(ticker, start_date, end_date):
-    # Create an instance of the class
-    AaplTicker = YahooTicker(ticker, start_date, end_date)
-    # Call the fetch_data() method
-    AaplTicker.fetch_data()
-
-    X_train, y_train, X_test, y_test, sc_extra = AaplTicker.prepare_data_feat_step(n_lags = 1, training_data_percent = 0.3, target_column='Open',extra_features=['Close'], reshape_for_lstm=True) #.   'Close'
-      
-    return(X_train, y_train, X_test, y_test, sc_extra)
-
-def select_model(model, X_train, y_train):
-    match model:
-        case 'LSTM':
-            return(setup_lstm(X_train, y_train))         
-        # If an exact match is not confirmed, this last case will be used if provided
-        case _:
-            return f'Sorrry, but {model} model not yet implemented.' 
-        
-@st.cache_data
-def setup_lstm(X_train, y_train):
-    lstm_model = lstm_horizon_model(X_train, y_train)
-    lstm_model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.3)
-    return lstm_model
-
-
-# @st.cache_data
-# def get_tickers():
-#     DATA_FILENAME = Path(__file__).parent/'data/nasdaq_tickers.csv'
-  
-#     try:
-#         # Fetch a list of stock tickers
-#         tickers_df = pd.read_csv(DATA_FILENAME)
-#         tickers_df.key = 'Symbol'
-#         return tickers_df
-#     except Exception as e:
-#         st.error(f"An error occurred while fetching tickers: {e}")
-#         return []
 
 def predict(ticker, model, begin_date, end_date):
     # tickers_dataset = fetch_data(ticker, begin_date, end_date)
