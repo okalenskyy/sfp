e1f00b13246f74f8dd3c9729777543af0572683c
Oleg Kalenskyy <57141635+okalenskyy@users.noreply.github.com>
2024-08-28 19:35:28 +0000

    new

--- main_app.py
+++ main_app.py
@@ -40,49 +40,51 @@ from st_on_hover_tabs import on_hover_tabs
 #     return y_pred_df, y_test, sc
 
 # ----[ NEW ]-------
-sfpUI:UI = object 
-
-def init_page():
+ 
+def ini_UI_adapter()->UI:
     #init UI object
-    sfpUI = UI('main')
+    sfpUI:UI = UI('main')
+    return(sfpUI)
+
+def init_page(adapter: UI):
+  
     # page---------
     st.set_page_config(
-        page_title=f'{sfpUI.title}',
-        page_icon=f'{sfpUI.icon}', 
+        page_title=f'{adapter.title}',
+        page_icon=f'{adapter.icon}', 
         layout='wide'
        )
-def init_side_bar():
+def init_side_bar(adapter: UI):
     # sidebar------
     with st.sidebar:
-        sfpUI.begin_date = st.date_input('Begin Date')
-        sfpUI.end_date = st.date_input('End Date')
+        adapter.begin_date = st.date_input('Begin Date')
+        adapter.end_date = st.date_input('End Date')
         
-        sfpUI.selected_ticker = st.selectbox('Ticker:', sfpUI.tickers)
+        adapter.selected_ticker = st.selectbox('Ticker:', adapter.tickers)
 
-        st.write(f'{sfpUI.selected_ticker_name}')
+        st.write(f'{adapter.selected_ticker_name}')
    
-        sfpUI.selected_model = pills('Select Model', sfpUI.models,sfpUI.icons)
-        sfpUI.pred_days = st.slider('Prediction days', 1, sfpUI.predict_days)
+        adapter.selected_model = pills('Select Model', adapter.models,adapter.icons)
+        adapter.pred_days = st.slider('Prediction days', 1, adapter.predict_days)
 
-def update_data():
- # run calculations ----
-    sfpUI.y_pred_df, sfpUI.y_test_df, sfpUI.sc = predict(sfpUI.selected_ticker, sfpUI.selected_model,'2024-01-01', '2024-08-01')
+def update_data(adapter: UI):
+ # run calculations ---- TODO
+    adapter.y_pred_df, adapter.y_test_df, sfpUI.sc = predict(adapter.selected_ticker, adapter.selected_model,'2024-01-01', '2024-08-01')
 
-def render_page():
+def render_page(adapter: UI):
     # header-------
-    st.markdown(f'## {sfpUI.icon} {sfpUI.title}')
+    st.markdown(f'## {adapter.icon} {adapter.title}')
     # st.markdown(f'{sfpUI.subtitle}')
-    
 
-    st.subheader(f'{sfpUI.selected_ticker_name}')
+    st.subheader(f'{adapter.selected_ticker_name}')
 
     col1, col2 = st.columns([3,1])
     with col1:
-        st.write(f'{sfpUI.selected_ticker_sector}')
-        st.write(f'{sfpUI.selected_ticker_industry}')
+        st.write(f'{adapter.selected_ticker_sector}')
+        st.write(f'{adapter.selected_ticker_industry}')
     with col2:
-        st.write(f'{sfpUI.selected_ticker_country}')
-        st.write(f'{sfpUI.selected_ticker_ipo_year}')
+        st.write(f'{adapter.selected_ticker_country}')
+        st.write(f'{adapter.selected_ticker_ipo_year}')
    
     # body--------
     tab_chart, tab_data, tab_model = st.tabs(["Chart", "Data", "Model"])
@@ -91,6 +93,7 @@ def render_page():
         st.header("Chart")
     
     with tab_data:
+
         st.header("Data")
         # st.dataframe(sfpUI.y_pred_df)
         
@@ -103,9 +106,10 @@ def render_page():
 # sfpUI.
 # predict(sfpUI.selected_ticker, sfpUI.begin_date, sfpUI.end_date)
 
-init_page()
-init_side_bar()
+sfpUI:UI = ini_UI_adapter()
+init_page(sfpUI)
+init_side_bar(sfpUI)
 # update_data
-render_page()
+render_page(sfpUI)
 
 
