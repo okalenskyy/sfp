f60b498fb2d66eb8dd368f118dc53d482aa0089b
Oleg Kalenskyy <57141635+okalenskyy@users.noreply.github.com>
2024-08-30 19:35:25 +0000

    e

--- Classes/UIClass.py
+++ Classes/UIClass.py
@@ -232,7 +232,7 @@ class UI():
         self.SelectedTicker.fetch_data()
     
     def prepare_datasets(self, training_data_percent:float = 0.2, n_lags:int=60, predict_days:int=0, target_column:str=None, extra_features=[], reshape_for_lstm:bool=False):
-        self.X_train, self.y_train, self.X_test, self.y_test, self.sc = self.SelectedTicker.prepare_data_feat_step(n_lags = 1, predict_days=5, training_data_percent = 0.3, target_column='Open',extra_features=['Close'], reshape_for_lstm=True) #.   'Close'
+        self.X_train, self.y_train, self.X_test, self.y_test, self.sc = self.SelectedTicker.prepare_data_feat_step(n_lags = 1, predict_days=predict_days, training_data_percent = training_data_percent, target_column=target_column, extra_features=extra_features, reshape_for_lstm=reshape_for_lstm) #.   'Close'
         # TODO
         
     def init_model(self):
